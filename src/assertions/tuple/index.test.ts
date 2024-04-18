c475117fe3e536781f251a7eee96060e4126207b
Yamiteru <yamiteru@icloud.com>
2024-04-19 01:20:13 +0200

    feat: add more tuple tests

--- src/assertions/tuple/index.test.ts
+++ src/assertions/tuple/index.test.ts
@@ -1,15 +1,29 @@
 import { boolean } from "@assertions/boolean";
 import { number } from "@assertions/number";
 import { string } from "@assertions/string";
-import { test } from "@fast-check/vitest";
+import { fc, test } from "@fast-check/vitest";
 import { expect } from "vitest";
 import { tuple } from "./index";
 
 const assertion = tuple([string, number, boolean]);
 
-test(() => {
-	expect(() => assertion(["1", 1, true])).not.toThrow();
-	expect(() => assertion(["1", "1", true])).toThrow();
-	expect(() => assertion(["1", 1])).toThrow();
-	expect(() => assertion(1)).toThrow();
-});
+test.prop([fc.tuple(fc.string(), fc.integer(), fc.boolean())])(
+	"should not throw if value passes all the assertions",
+	(value) => {
+		expect(() => assertion(value)).not.toThrow();
+	},
+);
+
+test.prop([fc.tuple(fc.string(), fc.string(), fc.boolean())])(
+	"should throw if value does not pass one of the assertions",
+	(value) => {
+		expect(() => assertion(value)).toThrow();
+	},
+);
+
+test.prop([fc.string()])(
+	"should throw if value is not of type array",
+	(value) => {
+		expect(() => assertion(value)).toThrow();
+	},
+);
